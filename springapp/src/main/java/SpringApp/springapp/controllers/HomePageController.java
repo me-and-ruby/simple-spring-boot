9959706ce911e954085406f13207370cfbeff057
joash <email@example.com>
2020-01-16 21:58:42 -0600

    added jsp

--- springapp/src/main/java/SpringApp/springapp/controllers/HomePageController.java
+++ springapp/src/main/java/SpringApp/springapp/controllers/HomePageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomePageController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/index")
 	public String homePage() {
 		return "index";
 	}
