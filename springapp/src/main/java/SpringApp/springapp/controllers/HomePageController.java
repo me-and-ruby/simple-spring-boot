ef4427dfdd495e05c447b90716bfbb957cbc6a82
joash <email@example.com>
2020-01-17 02:18:53 -0600

    added jsp page and rendered view

--- springapp/src/main/java/SpringApp/springapp/controllers/HomePageController.java
+++ springapp/src/main/java/SpringApp/springapp/controllers/HomePageController.java
@@ -1,14 +1,15 @@
 package SpringApp.springapp.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class HomePageController {
 	
 	@RequestMapping("/index")
 	public String homePage() {
-		return "index";
+		return "index.jsp";
 	}
 
 }
--- springapp/src/main/webapp/index.jsp
+++ springapp/src/main/webapp/index.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-  <h1>hello from index jsp </h1>
+ <h1>Calling from jsp page</h1>
 </body>
 </html>
